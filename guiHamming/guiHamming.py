b88e3cd0ed0fbd7719884ab9a9d5fe134377225f
Jose David <jose.david.san@gmail.com>
2021-03-19 15:39:09 -0600

    Hamming Gui
    Hamming gui started.

--- guiHamming/guiHamming.py
+++ guiHamming/guiHamming.py
@@ -4,10 +4,11 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.checkbox import CheckBox
 from kivy.core.window import Window
 
 # Set the app size
-Window.size = (600,450) #(x,y) lengths
+Window.size = (900,900) #(x,y) lengths
 
 class MyGridLayout(GridLayout):
     def __init__(self, **kwargs):
@@ -16,68 +17,100 @@ class MyGridLayout(GridLayout):
 
         self.cols = 1 # Set MyGridLayout columns
         self.row_force_default = True
-        self.row_default_height = 150
+        self.row_default_height = 75
         self.col_force_default = True
-        self.col_default_width = 600
+        self.col_default_width = 900
 
-        # Create a second gridlayout
-        self.topGrid = GridLayout(
+        #Create titel grid
+        self.titelGrid = GridLayout(
             row_force_default = True,
-            row_default_height = 50,
+            row_default_height = 75,
+            col_force_default = True,
+            col_default_width = 900)
+        self.titelGrid.cols = 1
+        
+        self.titelGrid.add_widget(Label(text="Aplicación Hamming", font_size = 48))
+
+        self.add_widget(self.titelGrid) # Add titelGrid to MyGridLayout
+
+        #Create body grid
+        self.bodyGrid = GridLayout(
+            row_force_default = True,
+            row_default_height = 30,
             col_force_default = True,
             col_default_width = 300)
-        self.topGrid.cols = 2 # Set topGrid number of cols
+        self.bodyGrid.cols = 3
 
-        # Add Label and Input Box to topGrid
-        self.topGrid.add_widget(Label(
-            text="Name: ",
-            font_size = 24))
-        self.name = TextInput(
-            multiline=False,
-            font_size = 24)
-        self.topGrid.add_widget(self.name)
+        # First row
+        self.bodyGrid.add_widget(Label(text="Ingrese su número binario:", font_size = 16))
+        self.binaryNumber = TextInput(multiline=False, font_size = 16)
+        self.bodyGrid.add_widget(self.binaryNumber)
+        
+        self.convertButton = Button(text="Convertir", font_size=16)
+        self.convertButton.bind(on_press = self.press) #Bond function
+        self.bodyGrid.add_widget(self.convertButton)
 
-        # Add Label and Input Box to topGrid
-        self.topGrid.add_widget(Label(
-            text="Pizza Favorita: ",
-            font_size = 24))
-        self.pizzaInput = TextInput(
-            multiline=False,
-            font_size = 24)
-        self.topGrid.add_widget(self.pizzaInput)
+        # Second row
+        self.decimalNumberLabel = Label(text="Su número en base decimal es:", font_size = 16)
+        self.bodyGrid.add_widget(self.decimalNumberLabel)
+        
+        self.octalNumberLabel = Label(text="Su número en base octal es:", font_size = 16)
+        self.bodyGrid.add_widget(self.octalNumberLabel)
+        
+        self.hexaNumberLabel = Label(text="Su número en base hexadecimal es:", font_size = 16)
+        self.bodyGrid.add_widget(self.hexaNumberLabel)
 
-        # Add Label and Input Box to topGrid
-        self.topGrid.add_widget(Label(
-            text="Color Favorito: ",
-            font_size = 24))
-        self.colorInput = TextInput(
-            multiline=False,
-            font_size = 24)
-        self.topGrid.add_widget(self.colorInput)
+        # Third row
+        self.decimalNumberValue = Label(text="----------------------------------", font_size = 16)
+        self.bodyGrid.add_widget(self.decimalNumberValue)
+        
+        self.octalNumberValue = Label(text="----------------------------------", font_size = 16)
+        self.bodyGrid.add_widget(self.octalNumberValue)
+        
+        self.hexaNumberValue = Label(text="----------------------------------", font_size = 16)
+        self.bodyGrid.add_widget(self.hexaNumberValue)
+        
+        # Add body grid to MyGridLayout
+        self.add_widget(self.bodyGrid)
 
-        # Add the topGrid to MyGridLayout
-        self.add_widget(self.topGrid)
+        # Division Label
+        self.add_widget(Label(text="___________________________________________________________________________________________________________________________________________________________",
+                              font_size = 16))
 
-        # Create a submit button
-        self.submit = Button(
-            text="Submit",
-            font_size=32)
-        self.submit.bind(on_press = self.press) #Bond function
-        self.add_widget(self.submit)
+        #Create graphic grid
+        self.graphicGrid = GridLayout(
+            row_force_default = True,
+            row_default_height = 30,
+            col_force_default = True,
+            col_default_width = 225)
+        self.graphicGrid.cols = 4
+        
+        #Create graphic grid first row
+        self.graphicGrid.add_widget(Label(text="Graficando código NZRI:", font_size = 16))
+        self.graphicGrid.add_widget(Label(text="Es par ?:", font_size = 16))
+        self.parityCheckBox = CheckBox()
+        self.graphicGrid.add_widget(self.parityCheckBox)
 
+        #Create graphic grid second row
+        self.graphingButton = Button(text="Graficar:", font_size=16)
+        self.graphingButton.bind(on_press = self.graphing) #Bond function
+        self.graphicGrid.add_widget(self.graphingButton)
+
+        self.add_widget(self.graphicGrid) # Add graphicGrid to MyGridLayout
+        
+    # Converting method   
     def press(self, instance):
-        name = self.name.text
-        pizza = self.pizzaInput.text
-        color = self.colorInput.text
+        binNum = self.binaryNumber.text # tipo de dato string
+    
+        self.decimalNumberValue.text = "234532" #convertirDecimal(binNum)
+        self.octalNumberValue.text = "57777"    #convertirOctal(binNum)
+        self.hexaNumberValue.text = "11AB3"     #convertirHexadecimal(binNum)
+        print("Llegué")
 
-        #print(f'Hola {name}, you like {pizza} pizza, and your favorite color is {color}!')
-        # Print in to the screen
-        self.add_widget(Label(text=f'Hola {name}, you like {pizza} pizza, and your favorite color is {color}!'))
-
-        # Clear inputs
-        self.name.text = ""
-        self.pizzaInput.text = ""
-        self.colorInput.text = ""
+    # Graphing method
+    def graphing(self, instance):
+        checkParityValue = self.parityCheckBox.active
+        print (checkParityValue)
         
 
 class MyApp(App):
